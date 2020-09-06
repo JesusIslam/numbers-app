d2b1f8cd128d8943e5754d68d7dd6daf35bde4eb
JesusIslam <omicron.b3ta@gmail.com>
2020-09-06 13:26:36 +0700

    add more tests

--- api/api.go
+++ api/api.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"fmt"
 	"io/ioutil"
 	"net/http"
 
@@ -12,10 +13,19 @@ func NumbersHandler(ctx echo.Context) error {
 
 	url := "http://numbersapi.com/" + number + "?json"
 
-	req, _ := http.NewRequest("GET", url, nil)
-	resp, _ := http.DefaultClient.Do(req)
-
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return fmt.Errorf("failed to create request: %v", err)
+	}
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("failed to do request: %v", err)
+	}
 	defer resp.Body.Close()
+
+	if resp.StatusCode >= http.StatusBadRequest {
+		return fmt.Errorf("invalid number parameter: %s", number)
+	}
 
 	body, _ := ioutil.ReadAll(resp.Body)
 	return ctx.JSON(http.StatusOK, string(body))
